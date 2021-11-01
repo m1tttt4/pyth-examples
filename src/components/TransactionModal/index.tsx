be4aa12260b395a82362ad3becd0721c52cff857
m1tttt4 <matthewhowen@gmail.com>
2021-11-01 01:14:49 -0400

    add working test token mint

--- src/components/TransactionModal/index.tsx
+++ src/components/TransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { Keypair, TransactionInstruction } from "@solana/web3.js";
+import { Keypair, LAMPORTS_PER_SOL, sendAndConfirmTransaction, Transaction, TransactionInstruction } from "@solana/web3.js";
 import { Token } from "@solana/spl-token";
 import { Pyth } from "../Icons/pyth";
 import type { Moment } from "moment";
@@ -357,6 +357,13 @@ export const TransactionModal = (props: TransactionModalProps) => {
     const mintWallet = Keypair.generate()
     const source = mintWallet.publicKey
     const source_account = Keypair.fromSecretKey(mintWallet.secretKey)
+    const fromAirdropSignature = await connection.requestAirdrop(
+      mintWallet.publicKey,
+      LAMPORTS_PER_SOL,
+    );
+    //wait for airdrop confirmation
+    await connection.confirmTransaction(fromAirdropSignature);
+
     const mint = await Token.createMint(
       connection,
       mintWallet,
@@ -365,7 +372,57 @@ export const TransactionModal = (props: TransactionModalProps) => {
       9,
       TOKEN_PROGRAM_ID,
     );
-    
+    //get the token account of the fromWallet Solana address, if it does not exist, create it
+    const fromTokenAccount = await mint.getOrCreateAssociatedAccountInfo(
+      mintWallet.publicKey,
+    );
+
+    // Generate a new wallet to receive newly minted token
+    var toWallet = Keypair.generate();
+
+    //get the token account of the toWallet Solana address, if it does not exist, create it
+    var toTokenAccount = await mint.getOrCreateAssociatedAccountInfo(
+      toWallet.publicKey,
+    );
+
+    //minting 1 new token to the "fromTokenAccount" account we just returned/created
+    await mint.mintTo(
+      fromTokenAccount.address, //who it goes to
+      mintWallet.publicKey, // minting authority
+      [], // multisig
+      1000000000, // how many
+    );
+
+    await mint.setAuthority(
+      mint.publicKey,
+      null,
+      "MintTokens",
+      mintWallet.publicKey,
+      []
+    )
+
+    // Add token transfer instructions to transaction
+    var transaction = new Transaction().add(
+      Token.createTransferInstruction(
+        TOKEN_PROGRAM_ID,
+        fromTokenAccount.address,
+        toTokenAccount.address,
+        mintWallet.publicKey,
+        [],
+        1,
+      ),
+    );
+
+    // Sign transaction, broadcast, and confirm
+    var signature = await sendAndConfirmTransaction(
+      connection,
+      transaction,
+      [mintWallet],
+      {commitment: 'confirmed'},
+    );
+    console.log('SIGNATURE', signature);
+
+
     // const escrow_mint_account = new PublicKey(escrow_mint)
     
     const pool = new Keypair()
