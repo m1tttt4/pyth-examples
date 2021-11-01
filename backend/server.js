e9093c2a1ef8f68dd21a21064008acae8b6e7735
Matthew Owen <matthewhowen@gmail.com>
2021-11-01 00:58:27 -0400

    add localhost to cors for db connection

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const socketPort = 8001;
 const server = require("http").createServer(app);
 const io = require("socket.io") (server, {
   cors: {
-    origin: "http://34.122.28.32:3000",
+    origin: [
+      "http://34.122.28.32:3000",
+      "http://localhost:3000"
+    ],
     methods: ["GET", "POST"],
   },
 });
